75afdad46120f2a317753e6651e84e4e5ab37f8a
Abhilash G <abhilash.ag1997@gmail.com>
2018-04-03 23:47:26 +0530

    directory clean-up

---
+++ CodeForces/Practice/Chtholly.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Chtholly {
+
+	public boollean isPalindromeAndZCY(long number){
+
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long k=scanner.nextLon(g), p=scanner.nextLong();
+
+		scanner.close();
+	}
+
+}
