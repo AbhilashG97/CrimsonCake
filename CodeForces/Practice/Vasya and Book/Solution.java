e3b0c29f896184f234b3d477222f38b781992b0d
AbhilashG97 <abhilash.ag1997@gmail.com>
2018-11-29 09:48:43 +0530

    CodeForces: A-Level Problem

---
+++ CodeForces/Practice/Vasya and Book/Solution.java
@@ -0,0 +1,48 @@
+// Problem link - https://codeforces.com/problemset/problem/1082/A
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-->0) {
+            int n = scanner.nextInt();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int d = scanner.nextInt();       
+            
+            int a = 0;
+            int N = 0;
+
+            StringBuilder numbers = new StringBuilder();
+
+            for(int i=1; i<n && a<y; i++) {
+                a = 1 + (i-1)*d;
+                numbers.append(a+","); 
+            }
+            
+            String numberString = numbers.toString();
+            //System.out.println(numberString);
+
+            if(numberString.contains(","+y+",")) {
+                if(numberString.contains(","+x+",")) {
+                    N = ((y-x)/d);
+                } else {
+                    N = ((y-1)/d);
+                    int count = 0;
+                    while(x>0) {
+                        x-=d;
+                        count++;
+                    }
+                    N+=count;
+                }
+            } else {
+                N=-1;
+            }
+            System.out.println(N);
+        }
+        scanner.close();
+    }
+}
