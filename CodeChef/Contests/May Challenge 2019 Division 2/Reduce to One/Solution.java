d136aff2801537d64d2be33953b78372c16adda2
AbhilashG97 <abhilash.ag1997@gmail.com>
2019-05-08 20:51:22 +0530

    Reduce To One: Partial Solve :cry:

--- CodeChef/Contests/May Challenge 2019 Division 2/Reduce to One/Solution.java
+++ CodeChef/Contests/May Challenge 2019 Division 2/Reduce to One/Solution.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.stream.*;
-import java.math.BigInteger;
 
 class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt();
         while(T-- > 0) {
-            ArrayList<BigInteger> numberList = new ArrayList<>();
+            ArrayList<Long> numberList = new ArrayList<>();
             int N = scanner.nextInt();
-            // numberList = Stream.iterate(1, i -> i + 1).limit(N)
-            //                    .collect(Collectors.toCollection(ArrayList::new));
-            for(int i=1; i<=N; i++) {
-                numberList.add(new BigInteger(String.valueOf(i)));
-            }
-            BigInteger result = new BigInteger("0");
+            numberList = Stream.iterate(1L, i -> i + 1).limit(N)
+                               .collect(Collectors.toCollection(ArrayList::new));
+  
+            long result = 0;
             if (numberList.size() == 1) {
-                result = new BigInteger("1");
+                result = 1;
             } else {
                 for(int i=0; i<numberList.size()-1; i+=2) {
                     if(i == numberList.size()-1) {
                         break;
                     }
-                    BigInteger X = numberList.get(i);
-                    BigInteger Y = numberList.get(i+1);
-                    result = (X.add(Y)).add(X.multiply(Y));
+                    long X = numberList.get(i);
+                    long Y = numberList.get(i+1);
+                    result = (X + Y + (X*Y))%1000000007;
                     numberList.add(result);
                     //System.out.println(numberList);
                 }
             }
-            System.out.println(result);
-            //System.out.println("\n\n\n");            
+            System.out.println(result);            
         }
         scanner.close();
     }
